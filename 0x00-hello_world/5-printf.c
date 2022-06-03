f7516598953428a712dafae1310255983dbde264
albericyao <yaoalberic@outlook.com>
2022-06-02 18:35:53 -0700

    upat

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) /*main funtion*/
+int main(void) /* main funtion */
 	{
 	/* Print function */
-	printf(stderr,"with proper grammar, but the outcome is a piece of art,\n");
+	printf(stderr, "with proper grammar, but the outcome is a piece of art, \n");
 
 	return (0);
 
