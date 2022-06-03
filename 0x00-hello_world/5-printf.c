355e960390e75cf92dff019fc58485c543e23f9b
albericyao <yaoalberic@outlook.com>
2022-06-02 18:26:16 -0700

    upat

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) /*main funtion*/
+	{
+	/* Print function */
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+	}
