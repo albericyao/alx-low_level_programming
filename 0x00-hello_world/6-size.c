a06d07a5c395b384e126b4ee5286aa095c5869cc
albericyao <yaoalberic@outlook.com>
2022-06-07 01:48:37 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  * return always 0
  */
 
-int main() {
+int main()
+{
     char char_type;
     int int_type;
     long int long_int_type;
     long long int long_long_int_type;
     float float_type;
-
     printf("Size of a char: %zu byte\(s\)\n", sizeof(char_type));
     printf("Size of an int: %zu byte\(s\)\n", sizeof(int_type));
     printf("Size of a long int: %zu byte\(s\)\n", sizeof(long_int_type));
-    printf("Size of a long long int: %zu byte\(s\)\n", sizeof(long_long_int_type));
+    printf("Size of a long long int: %zu byte\(s\)\n", sizeof(long_l_int_type));
     printf("Size of a float: %zu byte\(s\)\n", sizeof(float_type));
-    return 0;
+    return (0);
 }
