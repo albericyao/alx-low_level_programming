93f53a1ade0248a92d9f5232356a729a3b755abf
albericyao <yaoalberic@outlook.com>
2022-06-07 01:41:16 +0100

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - function for print size
+ *
+ * return always 0
+ */
+
+int main() {
+    char char_type;
+    int int_type;
+    long int long_int_type;
+    long long int long_long_int_type;
+    float float_ype;
+
+    printf("Size of a char: %zu byte\n", sizeof(char_type));
+    printf("Size of an int: %zu bytes\n", sizeof(int_type));
+    printf("Size of a long int: %zu bytes\n", sizeof(long_int_type));
+    printf("Size of a long long int: %zu bytes\n", sizeof(long_long_int_type));
+    printf("Size of a float: %zu bytes\n", sizeof(floatType));
+    return 0;
+}
