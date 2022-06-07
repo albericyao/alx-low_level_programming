7fa6b34c299da8994c0707c767e10507cbe3b5f0
albericyao <yaoalberic@outlook.com>
2022-06-07 01:50:00 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main()
     char char_type;
     int int_type;
     long int long_int_type;
-    long long int long_long_int_type;
+    long long int long_l_int_type;
     float float_type;
     printf("Size of a char: %zu byte\(s\)\n", sizeof(char_type));
     printf("Size of an int: %zu byte\(s\)\n", sizeof(int_type));
