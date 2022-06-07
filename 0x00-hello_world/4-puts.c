23e707ebe7d7f5441e4bf5a60f2fe8729cb3475c
albericyao <yaoalberic@outlook.com>
2022-06-06 17:02:11 -0700

    upat

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+
 /*
  * main funtion
 */
 int main(void)
 	{
-	/* Print function */
 	char str1[] = "\"programming is like building a multilingual puzzle";
 
 	puts(str1);
 
-	return (0);
+	return (1);
 
 	}
