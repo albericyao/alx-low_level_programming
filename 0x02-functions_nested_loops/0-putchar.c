81f382430ef5b53410e1b539f3e6bd935d35f814
albericyao <yaoalberic@outlook.com>
2022-06-07 18:26:40 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	char out_put[8]="_putchar";
+	char out_put[8] = "_putchar";
 	int i;
+
 	for (i = 0; i < 8; i++)
+	{
 		_putchar(out_put[i]);
-	_putchar('\n');
+		_putchar('\n');
+	}
 	return (0);
 }
