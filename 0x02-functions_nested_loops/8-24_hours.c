b098486de60174a8e71fdb7b7d155587e6ee4d1d
albericyao <yaoalberic@outlook.com>
2022-06-08 14:47:45 -0700

    abs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print time
+ * Description: function returns time
+ * Return: notthing
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 24; i++)
+		for (j = 0; j <= 59; i++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i);
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar('j');
+		}
+}
